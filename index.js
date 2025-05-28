aa6f3e32fa38a092c8d3bf56f0d8dfd225b56d97
Pradeep <pradeepgsv51@gmail.com>
2025-05-28 20:39:40 +0530

    Initialized Node Project, added dependencies and Started Server

---
+++ index.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Home Route");
+});
+
+function main() {
+  try {
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}.`);
+    });
+  } catch (err) {
+    console.log("Error : ", err);
+    process.exit(1);
+  }
+}
+
+main();
